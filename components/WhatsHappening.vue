5e63a7df75988bd3b2162a8786faabe06a8e0b84
Mert Şişmanoğlu <mert190737fb@gmail.com>
2023-04-19 11:30:14 +0300

    fix: Change Everyone can 'replay' to 'reply'

--- components/WhatsHappening.vue
+++ components/WhatsHappening.vue
@@ -49,7 +49,7 @@
                             <path d="M12 1.75C6.34 1.75 1.75 6.34 1.75 12S6.34 22.25 12 22.25 22.25 17.66 22.25 12 17.66 1.75 12 1.75zm-.25 10.48L10.5 17.5l-2-1.5v-3.5L7.5 9 5.03 7.59c1.42-2.24 3.89-3.75 6.72-3.84L11 6l-2 .5L8.5 9l5 1.5-1.75 1.73zM17 14v-3l-1.5-3 2.88-1.23c1.17 1.42 1.87 3.24 1.87 5.23 0 1.3-.3 2.52-.83 3.61L17 14z"></path>
                         </g>
                     </svg>
-                    Everyone can replay
+                    Everyone can reply
                     <span class="flex flex-col text-normalWhite gap-2 py-1 z-100 top-7 -right-12 rounded-xl absolute bg-black shadow-[rgba(222,222,222,0.6)] shadow-[0_0_7px_0] w-72 opacity-0 group-focus:opacity-100 pointer-events-none group-focus:pointer-events-auto transition duration-200">
                         <span class="text-sm font-light text-start text-lowsWhite px-2 py-2 ">
                             <b class="text-normalWhite font-bold">Who can reply?</b><br>
